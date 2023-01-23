741bd24e55bdd06b1356aec36e6143a34f25516f
Jinhee Han <jinheehanaaa@gmail.com>
2023-01-23 16:24:38 -0600

    004 - Implement Login Function

--- NOTE/note-taking/004 - Login.md
+++ NOTE/note-taking/004 - Login.md
@@ -4,5 +4,10 @@
 # Learn
 ## Install JWT Package
 
+## Implement JWT Helper Function
+
+## Login Function
+
+
 
 # Resources
--- controller/authController.go
+++ controller/authController.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jinheehanaaa/go-blog-backend/database"
 	"github.com/jinheehanaaa/go-blog-backend/models"
+	"github.com/jinheehanaaa/go-blog-backend/util"
 )
 
 func validateEmail(email string) bool {
@@ -62,5 +65,44 @@ func Register(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
 		"user":    user,
 		"message": "Account created successfully",
+	})
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		fmt.Println("Unable to parse body")
+	}
+	var user models.User
+	database.DB.Where("email=?", data["email"]).First(&user)
+	if user.Id == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "Email Address doesn't exist, please create an account",
+		})
+	}
+	if err := user.ComparePassword(data["password"]); err != nil {
+		c.Status(400) // Bad request
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+	token, err := util.GenerateJwt(strconv.Itoa(int(user.Id)))
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return nil
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message": "You are successfully logged in",
+		"user":    user,
 	})
 }
